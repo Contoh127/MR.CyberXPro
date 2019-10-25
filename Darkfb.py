d72e58067a9516c56a3206dbda6b43f9b0b648f7
Contoh127 <56506131+Contoh127@users.noreply.github.com>
2019-10-25 17:50:36 +0700

    Add files via upload

---
+++ Darkfb.py
@@ -0,0 +1,87 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merahji
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# [SUBSCRIBE] ESSA CHRISTIAN 
+# [Fixed By] Mr. CyberXPro
+clear
+echo  "SEBELUM MENGGUNAKAN TOOLS INI WAJIB INSTALL BAHAN DULU,, UNTUK INSTALL BAHAN PILIH NOMOR 0" | lolcat
+sleep 4
+clear
+figlet -f standard FB TOOLS | lolcat
+echo
+echo  " ____________________-=[Premium Tools]=-____________________" | lolcat
+echo  "|                                                           |" | lolcat
+echo  "|              Root®Author     : ESSA CHRISTIAN             |" | lolcat
+echo  "|              Whatsapp        : 089506092468              |" | lolcat
+echo  "|              Fixed By        : Mr. CyberXPro                 |" | lolcat
+echo  "|              Thanks to       : Mr. CyberXPro                 |" | lolcat
+echo  "|_______________________-=[Wellcome]=-______________________|" | lolcat
+date | lolcat
+echo
+echo $i"========================================"
+echo           " SCRIIPT TOOL HACK BY ESSA CHRISTIAN "       
+echo $i"========================================"
+echo $i"|"$me" 0"$i" |"$pu" INSTALL BAHAN DULU BRO"
+echo $i"========================================"
+echo $i"|"$me" 1"$i" |"$pu" BRUTEFORCE FB PHP"
+echo $i"|"$me" 2"$i" |"$pu" BRUTEFORCE FB v.1"
+echo $i"|"$me" 3"$i" |"$pu" BRUTEFORCE FB v.1.2"
+echo $i"|"$me" 4"$i" |"$pu" BRUTEFORCE FB v.1.3"
+echo $i"|"$me" 5"$i" |"$pu" BRUTEFORCE FB v.1.4"
+echo $i"|"$me" 6"$i" |"$pu" BRUTEFORCE FB "$me"[Required Root Access]"
+echo $i"────────────────────────────────"
+echo $i"|"$me" 7"$i" |"$pu" YAHOO CLONING FACEBOOK"
+echo $i"|"$me" 8"$i" |"$pu" PROFILE GUARD FACEBOOK"
+echo $i"|"$me" 9"$i" |"$pu" AUTO REPORT FACBOOK"
+echo $i"|"$me"10"$i" |"$pu" AUTO REACTION FACEBOOK"
+echo $i"|"$me"11"$i" |"$pu" AUTO LIKE FACEBOOK"
+echo $i"|"$me"12"$i" |"$pu" AUTO KOMEN FACEBOOK"
+echo $i"|"$me"13"$i" |"$pu" INFORMATION FACEBOOK"
+echo $i"────────────────────────────────"
+echo $i"|"$me"14"$i" |"$pu" PHISING FACEBOOK 1"
+echo $i"|"$me"15"$i" |"$pu" PHISING FACEBOOK 2"
+echo $i"|"$me"16"$i" |"$pu" PHISING FACEBOOK 3"
+echo $i"|"$me"17"$i" |"$pu" PHISING FACEBOOK 4"
+echo $i"========================================"
+echo $i"|"$me"18"$i" |"$pu" EXIT"
+echo $i"========================================"
+echo
+echo $me"┌==="$bi"["$i"ESSA CHRISTIAN"$bi"]"$me"======"$bi"["$i""Masukan Nomor""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 0 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install bs4
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " SUCCESS "|lolcat
+fi
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/Contoh127
+cd DarkFb
+python2 DarkFB.py
